182ce93afccb46137c350cd1ec7d0d2a036882c9
Dzsek <dzsekeb@gmail.com>
2021-04-06 20:16:09 +0300

    added highlight to unfinished items

---
+++ UnfinishedColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using dcsdbeditor.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace dcsdbeditor
+{
+    public class UnfinishedColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var o = value as TinyObject;
+            return o.id == o.name ? Brushes.Red : Brushes.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
